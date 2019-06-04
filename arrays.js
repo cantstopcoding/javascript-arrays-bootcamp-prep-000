fa2941504ba256150cef5caedc51a59d11109a7d
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-04 23:54:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
-
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
 
 
